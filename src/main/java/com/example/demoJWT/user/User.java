b0e218f17f0318f2b6a8c51e783ebfa3ecd1594e
haminhsang1903 <adventurer15999@gmail.com>
2020-09-27 08:40:42 +0700

    demo jwt

---
+++ src/main/java/com/example/demoJWT/user/User.java
@@ -0,0 +1,74 @@
+package com.example.demoJWT.user;
+/*******************************************************
+ * For Vietnamese readers:
+ *    Các bạn thân mến, mình rất vui nếu project này giúp 
+ * ích được cho các bạn trong việc học tập và công việc. Nếu 
+ * bạn sử dụng lại toàn bộ hoặc một phần source code xin để 
+ * lại dường dẫn tới github hoặc tên tác giá.
+ *    Xin cảm ơn!
+ *******************************************************/
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+/**
+ * Copyright 2019 {@author Loda} (https://loda.me).
+ * This project is licensed under the MIT license.
+ *
+ * @since 4/30/2019
+ * Github: https://github.com/loda-kun
+ */
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    
+    @Column(nullable = false, unique = true)
+    private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(Long id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public User() {
+		super();
+	}
+    
+}
